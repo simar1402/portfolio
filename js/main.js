6117a28f4d00ffa670331b1dd2fb3cb7daf05599
simar1402 <64465827+simar1402@users.noreply.github.com>
2021-11-24 18:20:30 +0530

    initial commit

---
+++ js/main.js
@@ -0,0 +1,26 @@
+// $(document).ready(function () {
+//   $(".main-bar").click(function (e) {
+//     $(this).toggleClass("activefile");
+//     e.preventDefault();
+//     $(".Nav_Wrapper").slideToggle();
+//   });
+//   $(".slider").owlCarousel({
+//     loop: true,
+//     margin: 0,
+//     autoplay: false,
+//     nav: true,
+//     autoplayTimeout: 4000,
+//     smartSpeed: 1500,
+//     responsive: {
+//       0: {
+//         items: 1,
+//       },
+//       600: {
+//         items: 1,
+//       },
+//       1000: {
+//         items: 1,
+//       },
+//     },
+//   });
+// });
